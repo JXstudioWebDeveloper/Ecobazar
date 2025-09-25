2eca78e9996688974d070586726ec27b270c6f2d
Jamshidbek Oydinov <jamshidoydinov929@gmail.com>
2025-09-25 13:16:43 +0500

    add

--- app.js
+++ app.js
@@ -25,3 +25,30 @@ function decrement() {
 plusBtn.addEventListener('click', increment);
 minusBtn.addEventListener('click', decrement);
 
+const btn1 = document.getElementById('tab1-btn');
+const btn2 = document.getElementById('tab2-btn');
+const btn3 = document.getElementById('tab3-btn');
+
+const tab1 = document.getElementById('tab1');
+const tab2 = document.getElementById('tab2');
+const tab3 = document.getElementById('tab3');
+const tab4 = document.getElementById('tab4');
+
+btn1.addEventListener('click', () => {
+  tab1.style.display = 'block';
+  tab2.style.display = 'none';
+  tab3.style.display = 'none';
+  tab4.style.display = 'block';
+});
+btn2.addEventListener('click', () => {
+  tab1.style.display = 'none';
+  tab2.style.display = 'block';
+  tab3.style.display = 'none';
+  tab4.style.display = 'block';
+});
+btn3.addEventListener('click', () => {
+  tab1.style.display = 'none';
+  tab2.style.display = 'none';
+  tab3.style.display = 'block';
+  tab4.style.display = 'none';
+});
